eedbb4782042b939afd025c1e39b4dd1a56b4979
SolArgentinoTV2025 <traplegionrecords2020@gmail.com>
2025-03-11 20:44:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -165,9 +165,9 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 const images = [
-    'E:/Anotate_en_Pasapalabra/pixlr-image-generator-e65dd46f-7f62-47ba-b30f-a9e5d01d8238.png',
-    'E:/Anotate_en_Pasapalabra/pixlr-image-generator-7b50e3dc-61f8-43ad-b4f6-517b6044d4f8.png',
-    'E:/Anotate_en_Pasapalabra/pixlr-image-generator-9c92ec6b-4fd1-4c67-af01-3b9de83cbc93.png'
+    'https://imperioinmobiliario.com.ar/pixlr-image-generator-e65dd46f-7f62-47ba-b30f-a9e5d01d8238.png',
+    'https://imperioinmobiliario.com.ar/pixlr-image-generator-7b50e3dc-61f8-43ad-b4f6-517b6044d4f8.png',
+    'https://imperioinmobiliario.com.ar/pixlr-image-generator-9c92ec6b-4fd1-4c67-af01-3b9de83cbc93.png'
 ];
 
 let currentImageIndex = 0;
